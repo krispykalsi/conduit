3ddd7b3163e019008c8df8ec4ffdf4f7e21f4911
Ikroop Singh Kalsi <ikroop.singh.kalsi@gmail.com>
2022-05-18 01:32:00 +0530

    Initial Commit

---
+++ conduitUITests/conduitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  conduitUITestsLaunchTests.swift
+//  conduitUITests
+//
+//  Created by Ikroop Singh Kalsi on 18/05/22.
+//
+
+import XCTest
+
+class conduitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
